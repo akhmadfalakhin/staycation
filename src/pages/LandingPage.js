7b575796bb8fec66a861cc852541254bb8549fe3
akhmadfalakhin <akhmadfalakhin22@gmail.com>
2020-12-13 11:56:41 +0700

    initial commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+import Header from 'parts/Header';
+import Hero from 'parts/Hero';
+import MostPicked from 'parts/MostPicked';
+import Categories from 'parts/Categories';
+import Testymony from 'parts/Testimony';
+import Footer from 'parts/Footer'
+
+import landingPage from 'json/landingPage.json';
+export default class LandingPage extends Component {
+    constructor(props){
+        super(props);
+        this.refMostPicked = React.createRef();
+    }
+    componentDidMount() {
+        window.title = "Staycation | Home";
+        window.scrollTo(0, 0);
+    }
+    render() {
+        return (
+        <>
+            <Header {...this.props}></Header>
+            <Hero 
+            refMostPicked={this.refMostPicked} 
+            data={landingPage.hero}/>
+            <MostPicked 
+            refMostPicked={this.refMostPicked}
+            data={landingPage.mostPicked}/> 
+            <Categories data={landingPage.categories}/>   
+            <Testymony data={landingPage.testimonial}/>
+            <Footer/>
+        </>
+        );
+    }
+}
